7e2925f22d48d557910abdc07fb00774bf6dce47
Raihan Bin Wahid <cse.rbwahid@gmail.com>
2022-05-19 03:08:14 +0600

    implement /todo post req

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
+const { MongoClient, ServerApiVersion } = require("mongodb");
 const jwt = require("jsonwebtoken");
 const express = require("express");
 const cors = require("cors");
-require("dotenv").config;
+require("dotenv").config();
 const port = process.env.PORT || 5000;
 const app = express();
 
@@ -11,5 +12,26 @@ app.use(cors());
 app.get("/", async (req, res) => {
   res.send({ success: true });
 });
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.sk2ow.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
+
+const run = async () => {
+  await client.connect();
+  const todoCollection = client.db("todo-list").collection("todos");
+
+  app.post("/todo", async (req, res) => {
+    const { todoData } = req.body;
+    const result = await todoCollection.insertOne(todoData);
+    res.send(result);
+  });
+ 
+};
+
+run().catch(console.dir);
 
 app.listen(port, () => console.log("listening at", port));
