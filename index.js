a2084ab914d4c5d8f86affc7fec98802224e0dbc
Raihan Bin Wahid <cse.rbwahid@gmail.com>
2022-05-19 05:54:26 +0600

    final

--- index.js
+++ index.js
@@ -36,17 +36,39 @@ const run = async () => {
   });
 
   app.put("/todo", async (req, res) => {
-    const { _id, isComplete, title, description } = req.body.todoData;
-    const filter = { _id: ObjectId(_id) };
-    const options = { upsert: true };
+    const { id, isComplete } = req.body;
+    const filter = { _id: ObjectId(id) };
+    const options = { upsert: false };
     const updateDoc = {
       $set: {
         isComplete,
-        title,
-        description,
       },
     };
     const result = await todoCollection.updateOne(filter, updateDoc, options);
+    res.send(result);
+  });
+
+  app.put("/todo/update", async (req, res) => {
+    const { id, title, description, isComplete } = req.body.todoData;
+    const filter = { _id: ObjectId(id) };
+    const options = { upsert: false };
+    const updateDoc = {
+      $set: {
+        title,
+        description,
+        isComplete,
+      },
+    };
+    const result = await todoCollection.updateOne(filter, updateDoc, options);
+    res.send(result);
+  });
+
+  app.delete("/todo/:id", async (req, res) => {
+    const { id } = req.params;
+    const query = { _id: ObjectId(id) };
+
+    const result = await todoCollection.deleteOne(query);
+
     res.send(result);
   });
 };
@@ -54,3 +76,4 @@ const run = async () => {
 run().catch(console.dir);
 
 app.listen(port, () => console.log("listening at", port));
+// 6285743c7e7048e97e5d18b0
